1f10f075ebb47b57c4cd9f1e7305cf960c56c1eb
Kavya Sivakumar <kavyaaaa58@gmail.com>
2026-08-17 12:24:55 +0530

    Add files via upload

---
+++ variables01.js
@@ -0,0 +1,17 @@
+function greetings() {
+    var myFriend = "Dharani";
+    return ("Greetings "+ myFriend);
+}
+console.log("result : " + greetings());
+
+
+let currentYear =  2026 ;
+console.log("current year result:" , currentYear);
+
+
+function testBestFlavor() {
+    const lacroix2 = "orange";
+    return ("Ana says, lacroix " + lacroix2 + "is a better flavor than lacroix " + lacroix1);
+}
+const lacroix1 = "grapefruit";
+console.log(testBestFlavor());
